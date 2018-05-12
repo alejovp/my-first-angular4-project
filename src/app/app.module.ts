fef4541a411af49396a9747d4c66a99b3053feb2
Alejandro <alejovp@gmail.com>
2018-05-12 16:04:22 +0200

    Changing Http service for the new HttpClient service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,5 @@
+import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
-import { HttpModule } from '@angular/http';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { AuthModule } from './auth/auth.module';
@@ -13,7 +13,9 @@ import { ShoppingListModule } from './shopping-list/shopping-list.module';
   ],
   imports: [
     BrowserModule,
-    HttpModule,
+    // HttpModule,
+    // Now with HttpClientModule
+    HttpClientModule,
     SharedModule,
     ShoppingListModule,
     // Now with Lazy loading...
--- src/app/shared/data-storage.service.ts
+++ src/app/shared/data-storage.service.ts
@@ -1,32 +1,65 @@
+import { HttpClient, HttpParams, HttpRequest } from "@angular/common/http";
 import { Injectable } from "@angular/core";
-import { Http, Response } from "@angular/http";
 import 'rxjs/Rx';
-
-import { RecipesService } from "../recipes/recipes.service";
-import { Recipe } from "../recipes/recipe.model";
 import { AuthService } from "../auth/auth.service";
+import { Recipe } from "../recipes/recipe.model";
+import { RecipesService } from "../recipes/recipes.service";
+
 
 @Injectable()
 export class DataStorageService {
   private dbUrl: string = 'https://ng-recipe-book-4a06b.firebaseio.com/';
 
-  constructor(private http: Http, 
-              private recipeService: RecipesService,
-              private authService: AuthService) {}
+  constructor(
+    // private http: Http,
+    // Now using HttpClient...
+    private httpClient: HttpClient,
+    private recipeService: RecipesService,
+    private authService: AuthService) {}
 
   storeRecipes() {
     const token = this.authService.getToken();
-
-    return this.http.put(`${this.dbUrl}recipes.json?auth=${token}`, this.recipeService.getRecipes());
+    // Using http:
+    // return this.http.put(`${this.dbUrl}recipes.json?auth=${token}`, this.recipeService.getRecipes());
+    
+    // Using httpClient opt1:
+    // return this.httpClient.put(`${this.dbUrl}recipes.json?auth=${token}`, this.recipeService.getRecipes());
+    
+    // Using httpClient opt2 (params):
+    // return this.httpClient.put(`${this.dbUrl}recipes.json`, 
+    //                             this.recipeService.getRecipes(),
+    //                             { params: new HttpParams().set('auth', token)});
+    
+    // Using httpCLient opt3 (httpRequest):
+    const req = new HttpRequest(
+                      'PUT', 
+                      `${this.dbUrl}recipes.json`, 
+                      this.recipeService.getRecipes(),
+                      { reportProgress: true, // This option give me info about the progress
+                        params: new HttpParams().set('auth', token) });
+    
+    return this.httpClient.request(req);
   }
 
   getStoredRecipes() {
     const token = this.authService.getToken();
 
-    return this.http.get(`${this.dbUrl}recipes.json?auth=${token}`)
+    // return this.http.get(`${this.dbUrl}recipes.json?auth=${token}`)
+    //   .map(
+    //     (response: Response) => {
+    //       const recipes: Recipe[] = response.json();
+    //       for (let recipe of recipes) {
+    //         if (!recipe['ingredients']) {
+    //           recipe.ingredients = []
+    //         }
+    //       }
+    //       return recipes;
+    //     }
+    //   )
+    return this.httpClient.get<Recipe[]>(`${this.dbUrl}recipes.json?auth=${token}`)
       .map(
-        (response: Response) => {
-          const recipes: Recipe[] = response.json();
+        // By default HttpCLient get only the body in response and use type JSON (all these can be changed)
+        (recipes) => {
           for (let recipe of recipes) {
             if (!recipe['ingredients']) {
               recipe.ingredients = []
